ddc0debe43cf2f6083af7e9a544a216a7f5eaea1
Yu Lagos Alfaro <betanae@gmail.com>
2023-05-13 12:54:21 -0400

    Elimina seccion productos

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,7 +1,6 @@
 <template>
 
   <NavBar/>
-  
   <div class="container">
 
     <div class="card" style="width: 50rem;">
@@ -9,7 +8,6 @@
       <div class="card-body ">
         <h5 class="card-title">Parece que te has perdido T.T</h5>
         <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
-        
         <div class="container-btn d-flex justify-content-center">
           <a href="/" class="btn btn-primary">Volver</a>
         </div>
